381582c2182c830e70ff2aea7a062342bc263eda
Rok Nikolič <rok.nikolic@gmail.com>
2024-02-21 20:51:04 +0100

    day 3 part 1 done

--- Go/Day 3 - Rucksack Reorganization.go
+++ Go/Day 3 - Rucksack Reorganization.go
@@ -6,10 +6,31 @@ import (
 	"strings"
 )
 
-func day3Part1(puzzleInput string) int {
-	//lines := strings.Split(puzzleInput, "\n")
+func commonLetter(string1 string, string2 string) int {
+	for _, letter1 := range string1 {
+		for _, letter2 := range string2 {
+			if letter1 == letter2 {
+				return int(letter1)
+			}
+		}
+	}
+	return -1
+}
 
-	return 0
+func day3Part1(puzzleInput string) int {
+	lines := strings.Split(puzzleInput, "\n")
+	totalSum := 0
+	for _, line := range lines {
+		middlePoint := len(line) / 2
+		first, second := line[:middlePoint], line[middlePoint:]
+		asciiLetter := commonLetter(first, second)
+		if asciiLetter > 96 {
+			totalSum += asciiLetter - 96
+		} else {
+			totalSum += asciiLetter - 64 + 26
+		}
+	}
+	return totalSum
 }
 
 func day3Part2(puzzleInput string) int {
