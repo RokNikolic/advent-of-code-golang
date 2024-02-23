5784f589de4ceaa3e1e08859f76f5cad01079030
Rok Nikolič <rok.nikolic@gmail.com>
2024-02-23 16:17:15 +0100

    day 3 part 2 done

--- Go/Day 3 - Rucksack Reorganization.go
+++ Go/Day 3 - Rucksack Reorganization.go
@@ -6,33 +6,27 @@ import (
 	"strings"
 )
 
-func stringIntersection(strings ...string) {
-
-}
-
-func stringIntersectionBasic(string1 string, string2 string) {
+func stringIntersection(strings ...string) []int {
+	firstString := strings[0]
+	strings = strings[1:]
 	workingSet := make(map[int32]bool)
-	for _, char := range string1 {
+	for _, char := range firstString {
 		workingSet[char] = true
 	}
-	intersectionSet := make(map[int32]bool)
-	for _, char := range string2 {
-		if _, found := workingSet[char]; found {
-			intersectionSet[char] = true
-		}
-	}
-}
-
-func commonLetter(string1 string, string2 string) []int {
-	var commonLetters []int
-	for _, letter1 := range string1 {
-		for _, letter2 := range string2 {
-			if letter1 == letter2 {
-				commonLetters = append(commonLetters, int(letter1))
+	for _, workString := range strings {
+		intersectionSet := make(map[int32]bool)
+		for _, char := range workString {
+			if _, found := workingSet[char]; found {
+				intersectionSet[char] = true
 			}
 		}
+		workingSet = intersectionSet
 	}
-	return commonLetters
+	var outputSlice []int
+	for key := range workingSet {
+		outputSlice = append(outputSlice, int(key))
+	}
+	return outputSlice
 }
 
 func getPriority(asciiChar int) int {
@@ -48,18 +42,23 @@ func day3Part1(puzzleInput string) int {
 	totalSum := 0
 	for _, line := range lines {
 		middlePoint := len(line) / 2
-		first, second := line[:middlePoint], line[middlePoint:]
-		asciiLetters := commonLetter(first, second)
-		fmt.Println(asciiLetters)
+		firstHalf, secondHalf := line[:middlePoint], line[middlePoint:]
+		asciiLetters := stringIntersection(firstHalf, secondHalf)
 		totalSum += getPriority(asciiLetters[0])
 	}
 	return totalSum
 }
 
 func day3Part2(puzzleInput string) int {
-	//lines := strings.Split(puzzleInput, "\n")
-
-	return 0
+	lines := strings.Split(puzzleInput, "\n")
+	totalSum := 0
+	for i := 0; i < len(lines)/3; i++ {
+		iter := i * 3
+		first, second, third := lines[iter], lines[iter+1], lines[iter+2]
+		asciiLetters := stringIntersection(first, second, third)
+		totalSum += getPriority(asciiLetters[0])
+	}
+	return totalSum
 }
 
 func day3() {
