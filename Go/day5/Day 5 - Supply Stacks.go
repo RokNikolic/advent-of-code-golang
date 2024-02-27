b4c1483f3a56f0b1996e0ac1bea43fb7349e550f
Rok Nikolič <rok.nikolic@gmail.com>
2024-02-27 17:14:19 +0100

    day 5 input processed

--- Go/day5/Day 5 - Supply Stacks.go
+++ Go/day5/Day 5 - Supply Stacks.go
@@ -3,10 +3,55 @@ package day5
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
+func transpose(slice [][]string) [][]string {
+	xl := len(slice[0])
+	yl := len(slice)
+	result := make([][]string, xl)
+	for i := range result {
+		result[i] = make([]string, yl)
+	}
+	for i := 0; i < xl; i++ {
+		for j := 0; j < yl; j++ {
+			result[i][j] = slice[j][i]
+		}
+	}
+	return result
+}
+
 func part1(puzzleInput string) int {
+	chunks := strings.Split(puzzleInput, "\n\n")
+	lines := strings.Split(chunks[0], "\n")
+	lines = lines[:len(lines)-1]
+
+	var stacks [][]string
+	for _, line := range lines {
+		var lineItems []string
+		for i := 0; i <= len(lines[len(lines)-1])/4; i++ {
+			line += "              "
+			lineItem := string(line[i*4+1])
+			lineItems = append(lineItems, lineItem)
+		}
+		stacks = append(stacks, lineItems)
+	}
+	transposedStacks := transpose(stacks)
+
+	var finalStacks [][]string
+	for _, stack := range transposedStacks {
+		var reversedStack []string
+		for _, element := range stack {
+			element = strings.Replace(element, " ", "0", -1)
+			if element != "0" {
+				reversedStack = append(reversedStack, element)
+			}
+		}
+		slices.Reverse(reversedStack)
+		finalStacks = append(finalStacks, reversedStack)
+	}
+	fmt.Println(finalStacks)
 	return 0
 }
 
@@ -15,7 +60,10 @@ func part2(puzzleInput string) int {
 }
 
 func Day5() {
-	puzzleRead, _ := os.ReadFile("Input/day5.txt")
+	puzzleRead, err := os.ReadFile("Input/day5.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
 	puzzleString := string(puzzleRead)
 	puzzleCleaned := strings.Replace(puzzleString, "\r", "", -1)
 
