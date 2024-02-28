964e020fc466cb63e3af14a3a7fac76f08ff5fb9
Rok Nikolič <rok.nikolic@gmail.com>
2024-02-28 12:23:14 +0100

    day 5 done, tough but fun

--- Go/day5/Day 5 - Supply Stacks.go
+++ Go/day5/Day 5 - Supply Stacks.go
@@ -3,7 +3,9 @@ package day5
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -22,23 +24,39 @@ func transpose(slice [][]string) [][]string {
 	return result
 }
 
-func part1(puzzleInput string) int {
-	chunks := strings.Split(puzzleInput, "\n\n")
-	lines := strings.Split(chunks[0], "\n")
-	lines = lines[:len(lines)-1]
+func moveStacks(stacks [][]string, instructions []int, oneByOne bool) [][]string {
+	amount := instructions[0]
+	source := instructions[1] - 1
+	destination := instructions[2] - 1
 
+	movingSlice := stacks[source][len(stacks[source])-amount:]
+	if oneByOne {
+		slices.Reverse(movingSlice)
+	}
+	stacks[source] = stacks[source][:len(stacks[source])-amount]
+	stacks[destination] = append(stacks[destination], movingSlice...)
+	return stacks
+}
+
+func partBoth(puzzleInput string, part int) string {
+	chunks := strings.Split(puzzleInput, "\n\n")
+	pallets := chunks[0]
+	instructions := chunks[1]
+
+	palletLines := strings.Split(pallets, "\n")
+	palletLines = palletLines[:len(palletLines)-1]
 	var stacks [][]string
-	for _, line := range lines {
+	for _, line := range palletLines {
 		var lineItems []string
-		for i := 0; i <= len(lines[len(lines)-1])/4; i++ {
-			line += "              "
+		for i := 0; i <= len(palletLines[len(palletLines)-1])/4; i++ {
+			line += "                                        "
 			lineItem := string(line[i*4+1])
 			lineItems = append(lineItems, lineItem)
 		}
 		stacks = append(stacks, lineItems)
 	}
-	transposedStacks := transpose(stacks)
 
+	transposedStacks := transpose(stacks)
 	var finalStacks [][]string
 	for _, stack := range transposedStacks {
 		var reversedStack []string
@@ -51,12 +69,31 @@ func part1(puzzleInput string) int {
 		slices.Reverse(reversedStack)
 		finalStacks = append(finalStacks, reversedStack)
 	}
-	fmt.Println(finalStacks)
-	return 0
-}
 
-func part2(puzzleInput string) int {
-	return 0
+	instructionLines := strings.Split(instructions, "\n")
+	for _, line := range instructionLines {
+		pattern := regexp.MustCompile("\\d+")
+		foundNumbers := pattern.FindAllString(line, -1)
+
+		var instructionNumbers []int
+		for _, number := range foundNumbers {
+			intNum, _ := strconv.Atoi(number)
+			instructionNumbers = append(instructionNumbers, intNum)
+		}
+		if part == 1 {
+			finalStacks = moveStacks(finalStacks, instructionNumbers, true)
+		} else if part == 2 {
+			finalStacks = moveStacks(finalStacks, instructionNumbers, false)
+		}
+	}
+
+	outputString := ""
+	for _, stack := range finalStacks {
+		topItem := stack[len(stack)-1]
+		outputString += topItem
+	}
+
+	return outputString
 }
 
 func Day5() {
@@ -67,6 +104,6 @@ func Day5() {
 	puzzleString := string(puzzleRead)
 	puzzleCleaned := strings.Replace(puzzleString, "\r", "", -1)
 
-	fmt.Printf("Day 5 Part 1 result is: %v\n", part1(puzzleCleaned))
-	fmt.Printf("Day 5 Part 2 result is: %v\n", part2(puzzleCleaned))
+	fmt.Printf("Day 5 Part 1 result is: %v\n", partBoth(puzzleCleaned, 1))
+	fmt.Printf("Day 5 Part 2 result is: %v\n", partBoth(puzzleCleaned, 2))
 }
